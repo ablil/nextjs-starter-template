e1abe682e08e4c0527c4fd037f998a8965ef4bcc
ablil <ablil@proton.me>
2025-06-15 12:37:13 +0100

    feat: reusable input component and style

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,11 +1,17 @@
 import ButtonWithIcon from "@/components/common/ButtonWithIcon"
-import { ArrowLeftEndOnRectangleIcon, ArrowRightCircleIcon } from "@heroicons/react/16/solid"
+import InputWithIcon from "@/components/common/InputWithIcon"
+import {
+  ArrowLeftEndOnRectangleIcon,
+  ArrowRightCircleIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/16/solid"
 import React from "react"
 
 const ComponentsPage = () => {
   return (
-    <main className="grid grid-cols-3 gap-4 p-8">
-      <div className="flex gap-4 flex-wrap">
+    <main className="p-12">
+      <h1 className="uppercase text-xl">buttons</h1>
+      <div className="flex gap-2 p-4 flex-wrap">
         <button>no style</button>
         <button className="filled">click me</button>
         <button className="filled" disabled={true}>
@@ -51,8 +57,54 @@ const ComponentsPage = () => {
           sign out
         </ButtonWithIcon>
       </div>
-      <div>
-        <input type="text" name="" id="" placeholder="type here" />
+      <h1 className="uppercase text-xl">inputs</h1>
+      <div className="flex gap-2 p-4 flex-wrap">
+        <input type="text" placeholder="no style" className="" />
+        <input type="text" placeholder="underlined" className="underlined" />
+        <input type="text" placeholder="filled underlined" className="underlined filled" />
+        <input type="text" placeholder="bordered" className="bordered" />
+        <input type="text" placeholder="bordered filled" className="bordered filled" />
+
+        <InputWithIcon
+          placeholder="underlined left icon"
+          className="underlined"
+          leftIcon={<MagnifyingGlassIcon className="size-4" />}
+        />
+        <InputWithIcon
+          placeholder="underlined right icon"
+          className="underlined"
+          rightIcon={<MagnifyingGlassIcon className="size-4" />}
+        />
+        <InputWithIcon
+          placeholder="underlined filled left icon"
+          className="underlined filled"
+          leftIcon={<MagnifyingGlassIcon className="size-4" />}
+        />
+        <InputWithIcon
+          placeholder="underlined filled right icon"
+          className="underlined filled"
+          rightIcon={<MagnifyingGlassIcon className="size-4" />}
+        />
+        <InputWithIcon
+          placeholder="bordered left icon"
+          className="bordered"
+          leftIcon={<MagnifyingGlassIcon className="size-4" />}
+        />
+        <InputWithIcon
+          placeholder="bordered right icon"
+          className="bordered"
+          rightIcon={<MagnifyingGlassIcon className="size-4" />}
+        />
+        <InputWithIcon
+          placeholder="bordered filled left icon"
+          className="bordered filled"
+          leftIcon={<MagnifyingGlassIcon className="size-4" />}
+        />
+        <InputWithIcon
+          placeholder="bordered filled right icion"
+          className="bordered filled"
+          rightIcon={<MagnifyingGlassIcon className="size-4" />}
+        />
       </div>
       <div>
         <h1>titles</h1>
