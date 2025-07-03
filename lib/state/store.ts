cb45d1a11f0a8997ae0c947c6ab3974d1759551e
ablil <ablil@proton.me>
2025-07-03 21:23:21 +0100

    refactor(redux): move typed hooks to hooks.ts and clean up store.ts
    - Created lib/state/hooks.ts for typed useAppDispatch and useAppSelector
    - Removed typed hooks from store.ts for better modularity and type safety
    - No usages found, ready for use in components

--- lib/state/store.ts
+++ lib/state/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { DefaultReducer } from "./slices/app"
-import { useDispatch, useSelector, useStore } from "react-redux"
 import createSagaMiddleware from "redux-saga"
 import { appSagasWatcher } from "./sagas/app"
 
@@ -20,7 +19,3 @@ export const makeStore = () => {
 export type AppStore = ReturnType<typeof makeStore>
 export type RootState = ReturnType<AppStore["getState"]>
 export type AppDispatch = AppStore["dispatch"]
-
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
-export const useAppSelector = useSelector.withTypes<RootState>()
-export const useAppStore = useStore.withTypes<AppStore>()
